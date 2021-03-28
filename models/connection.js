1d69f791d3f58872c6268b23028ed542dd44ae86
Yaya <yayazdiallo@outlook.fr>
2021-03-28 17:54:25 +0200

    Change name bdd

--- models/connection.js
+++ models/connection.js
@@ -10,12 +10,13 @@ const options = {
 // URI_BDD Credentials
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
-const dbname = 'morningnews-correction';
+const dbname = process.env.DB_NAME;
 const URI_BDD = `mongodb+srv://${username}:${password}@cluster0.9rdyy.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 
 try {
   // Connect to the MongoDB cluster
   mongoose.connect(URI_BDD, options, () => {
+    console.log(dbname);
     console.log(' Mongoose is connected');
   });
 } catch (e) {
